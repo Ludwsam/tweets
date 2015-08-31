d66dc359264fb45e0e55b46f57f4b2579dffc5f8
Ludwine Probst <ludwine.probst@gmail.com>
2015-08-31 22:15:40 +0200

    clean

--- src/main/java/com/springone/spark/NGram.java
+++ src/main/java/com/springone/spark/NGram.java
@@ -10,18 +10,6 @@ import java.util.List;
  */
 public class NGram {
 
-  public static Iterable<String> ngramsword(int n, String str) {
-    List<String> ngrams = new ArrayList<>();
-    String[] words = str.split(" ");
-
-    for (int i = 0; i < words.length - n + 1; i++) {
-      //ngrams.add(concat(words, i, i + n));
-    }
-
-    return ngrams;
-
-  }
-
   public static Iterable<String> ngrams(int n, String str) {
     List<String> ngrams = new ArrayList<>();
     char[] words = str.toCharArray();
@@ -42,16 +30,6 @@ public class NGram {
 
     return sb.toString();
   }
-
-  /*public static String concat(String[] words, int start, int end) {
-    StringBuilder sb = new StringBuilder();
-
-    for (int i = start; i < end; i++) {
-      sb.append((i > start ? " " : "") + words[i]);
-    }
-
-    return sb.toString();
-  }*/
 
   public static void main(String[] args) {
 
